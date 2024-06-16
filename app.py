c3f0f6cf57f2ab6344a5c540cbe0a1fc8c4d2892
David Ramon Nunes <62158877+David1r20@users.noreply.github.com>
2024-06-16 19:16:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ def main():
     #file_path = 'Power_data.csv'
     #uploaded_file = pd.read_csv(file_path)
     # Upload do arquivo CSV
-    uploaded_file = st.file_uploader("Escolha um arquivo CSV", type="csv")
+    #uploaded_file = st.file_uploader("Escolha um arquivo CSV", type="csv")
     
     if uploaded_file is not None:
-        data = pd.read_csv(uploaded_file)
+        data = pd.read_csv('Power_data.csv')
 
         # Mostrar os primeiros registros do dataframe
         st.write("Primeiros registros do dataframe:")
