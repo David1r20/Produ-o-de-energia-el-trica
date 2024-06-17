8e07cd758f806a115f7e38ad1fcdf33c60bd7e1f
David Ramon Nunes <62158877+David1r20@users.noreply.github.com>
2024-06-17 02:41:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def main():
     try:
         st.subheader("Dados do Dataset")
         st.write(data.head())
-
+    
         # Visualização das distribuições das variáveis
         st.subheader("Distribuição das Variáveis")
         fig, axes = plt.subplots(2, 2, figsize=(14, 10))
@@ -55,7 +55,7 @@ def main():
         sns.histplot(data['Relative humidity'], kde=True, ax=axes[1, 1])
         axes[1, 1].set_title('Distribuição da Umidade Relativa')
         st.pyplot(fig)
-
+    
         # Matriz de correlação com mapa de calor
         st.subheader("Matriz de Correlação")
         corr_matrix = data.corr()
@@ -63,60 +63,60 @@ def main():
         sns.heatmap(corr_matrix, annot=True, cmap='coolwarm', fmt=".2f")
         plt.title('Mapa de Calor da Matriz de Correlação')
         st.pyplot()
-
+    
         # Definir variáveis independentes (X) e dependente (y)
         X = data[['Avg temperature', 'Exhaust vacuum', 'Ambient pressure', 'Relative humidity']]
         y = data['Net hourly electrical energy output']
-
+    
         # Normalizar os dados
         scaler = StandardScaler()
         X_scaled = scaler.fit_transform(X)
-
+    
         # Dividir os dados em conjuntos de treino e teste (80% treino, 20% teste)
         X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.25, random_state=45)
-
+    
         # Definir valores fixos para alpha e parâmetros dos modelos
         alpha_lasso = 0.01
         alpha_ridge = 0.2
         alpha_elastic = 0.01
         l1_ratio_elastic = 0.0
-
+    
         # Configurar modelos Lasso, Ridge e ElasticNet
         lasso = Lasso(alpha=alpha_lasso)
         ridge = Ridge(alpha=alpha_ridge)
         elastic = ElasticNet(alpha=alpha_elastic, l1_ratio=l1_ratio_elastic)
-
+    
         # Ajustar os modelos
         lasso.fit(X_train, y_train)
         ridge.fit(X_train, y_train)
         elastic.fit(X_train, y_train)
-
+    
         # Avaliar os modelos ajustados
         y_pred_lr = lasso.predict(X_test)
         y_pred_ridge = ridge.predict(X_test)
         y_pred_elastic = elastic.predict(X_test)
-
+    
         mse_lr = mean_squared_error(y_test, y_pred_lr)
         mse_ridge = mean_squared_error(y_test, y_pred_ridge)
         mse_elastic = mean_squared_error(y_test, y_pred_elastic)
-
+    
         r2_lr = r2_score(y_test, y_pred_lr)
         r2_ridge = r2_score(y_test, y_pred_ridge)
         r2_elastic = r2_score(y_test, y_pred_elastic)
-
+    
         st.subheader("Avaliação dos Modelos Ajustados")
         st.write(f"**Modelo Lasso Regression (Alpha: {alpha_lasso}):**")
         st.write(f"   Mean Squared Error (MSE): {mse_lr:.2f}")
         st.write(f"   R-squared (R2): {r2_lr:.2f}")
-
+    
         st.write(f"**Modelo Ridge Regression (Alpha: {alpha_ridge}):**")
         st.write(f"   Mean Squared Error (MSE): {mse_ridge:.2f}")
         st.write(f"   R-squared (R2): {r2_ridge:.2f}")
-
+    
         st.write(f"**Modelo Elastic Net Regression (Alpha: {alpha_elastic}, L1 Ratio: {l1_ratio_elastic}):**")
         st.write(f"   Mean Squared Error (MSE): {mse_elastic:.2f}")
         st.write(f"   R-squared (R2): {r2_elastic:.2f}")
-
+    
         st.write("""
         Os modelos Lasso, Ridge e Elastic Net apresentaram um desempenho excelente na previsão da produção de 
         energia elétrica, com valores de Mean Squared Error (MSE) próximos, variando de 21.10 a 21.18, e um R-squared (R2) 
@@ -124,7 +124,7 @@ def main():
         um desempenho idêntico, enquanto o Elastic Net, com uma leve ênfase em regularização L2, teve um MSE ligeiramente maior. Em resumo, 
         todos os modelos são eficazes e bem ajustados, mostrando alta precisão e capacidade de previsão para as condições ambientais e operacionais analisadas.
         """)
-
+    
         # Widgets para entrada de parâmetros de previsão
         st.sidebar.header("Parâmetros de Previsão")
         temperature = st.sidebar.slider("Temperatura Média (°C)", min_value=0, max_value=60, value=25)
@@ -139,24 +139,24 @@ def main():
             'Ambient pressure': [pressure],
             'Relative humidity': [humidity]
         })
-
+    
         # Normalizar os dados de entrada
         input_data_scaled = scaler.transform(input_data)
-
+    
         # Fazer previsões com os modelos ajustados
         predicted_energy_output_lasso = lasso.predict(input_data_scaled)[0]
         predicted_energy_output_ridge = ridge.predict(input_data_scaled)[0]
         predicted_energy_output_elastic = elastic.predict(input_data_scaled)[0]
-
+    
         st.subheader("**Previsões de Produção de Energia em Tempo Real**")
         st.markdown(f"### **Modelo Lasso Regression**", unsafe_allow_html=True)
         st.markdown(f"<h1 style='text-align: center;'>Previsão: <span style='color: red;'>{predicted_energy_output_lasso:.2f} MWh</span></h1>", unsafe_allow_html=True)
         st.markdown(f"### **Modelo Ridge Regression**", unsafe_allow_html=True)
         st.markdown(f"<h1 style='text-align: center;'>Previsão: <span style='color: red;'>{predicted_energy_output_ridge:.2f} MWh</span></h1>", unsafe_allow_html=True)
-
+    
         st.markdown(f"### **Modelo Elastic Net Regression**", unsafe_allow_html=True)
         st.markdown(f"<h1 style='text-align: center;'>Previsão: <span style='color: red;'>{predicted_energy_output_elastic:.2f} MWh</span></h1>", unsafe_allow_html=True)
-
+    
         # Seção de previsão mensal
         st.subheader("Previsão Mensal com Valores Aleatórios")
         days = np.arange(1, 31)
@@ -164,20 +164,20 @@ def main():
         vacuums = np.random.uniform(vacuum - 10, vacuum + 10, size=30)
         pressures = np.random.uniform(pressure - 20, pressure + 20, size=30)
         humidities = np.random.uniform(humidity - 20, humidity + 20, size=30)
-
+    
         monthly_data = pd.DataFrame({
             'Avg temperature': temperatures,
             'Exhaust vacuum': vacuums,
             'Ambient pressure': pressures,
             'Relative humidity': humidities
         })
-
+    
         monthly_data_scaled = scaler.transform(monthly_data)
-
+    
         predictions_lasso = lasso.predict(monthly_data_scaled)
         predictions_ridge = ridge.predict(monthly_data_scaled)
         predictions_elastic = elastic.predict(monthly_data_scaled)
-
+    
         fig, ax = plt.subplots()
         ax.plot(days, predictions_lasso, label='Lasso Regression', color='blue')
         ax.plot(days, predictions_ridge, label='Ridge Regression', color='red')
