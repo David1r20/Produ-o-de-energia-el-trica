d21b53ed46fbe09c8d3873890cee263971bca1d7
David Ramon Nunes <62158877+David1r20@users.noreply.github.com>
2024-06-17 02:03:54 -0300

    Update app.py

--- app.py
+++ app.py
@@ -30,9 +30,9 @@ def main():
         X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.25, random_state=45)
 
         # Definir valores fixos para alpha e parâmetros dos modelos
-        alpha_lasso = 0.0001
-        alpha_ridge = 0.562
-        alpha_elastic = 0.0001
+        alpha_lasso = 0.01
+        alpha_ridge = 0.2
+        alpha_elastic = 0.01
         l1_ratio_elastic = 0.0
 
         # Configurar modelos Lasso, Ridge e ElasticNet
